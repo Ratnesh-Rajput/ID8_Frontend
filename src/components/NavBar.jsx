4c203dc842f95bb8369d75a4a4e6295e3f0df222
Ratnesh-Rajput <105653121+Ratnesh-Rajput@users.noreply.github.com>
2025-12-17 04:00:00 +0530

    setup: Tailwind & ShadCn

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+
+
+function NavBar() {
+    return (
+       <>
+       <div>
+        ID8
+       </div>
+       <div>
+        Home
+       </div>
+        <div>
+        Create Note
+        </div>
+       </> 
+    )
+}
+
+export default NavBar
